c8bda6f3ad468670031d66652b19d42e08d64df3
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-03-30 14:57:00 -0600

    test

--- watch-that-box/javascript.js
+++ watch-that-box/javascript.js
@@ -1,12 +1,26 @@
   var size = 250;
   var box = document.getElementById("box");
-document.getElementById("grow").addEventListener("click", function(){
+box.addEventListener("wheel", function(event){
+  if(event.deltaY < 0){
+    size = size * 1.25;
+  } else{
+      size = size / 1.25;
+  }
   box.style.height = size+"px"; 
   box.style.width = size+"px";
-  size = size * 1.25;
 });
 
-document.getElementById("blue").addEventListener("click", function(){
+document.getElementById("grow").addEventListener("click", bigger);
+
+//document.getElementById("grow").addEventListener("click", function(){
+  //box.style.height = size+"px"; 
+ // box.style.width = size+"px";
+ //size = size * 1.25;
+//}); 
+
+
+document.getElementById("blue").addEventListener("click", function(e){
+  console.log(e);
   box.style.backgroundColor = "blue";
 });
 
@@ -21,3 +35,12 @@ document.getElementById("reset").addEventListener("click", function(){
   box.style.opacity = "1.0";
   size = 250;
 });
+
+function bigger()
+{
+  box.style.height = size+"px"; 
+  box.style.width = size+"px";
+  size = size * 1.25;    
+  };
+
+ 
