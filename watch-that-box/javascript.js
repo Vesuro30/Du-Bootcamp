5f0978030a15460383c6ff43a6e797198aa7677f
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-03-30 13:42:39 -0600

    box

--- watch-that-box/javascript.js
+++ watch-that-box/javascript.js
@@ -1,19 +1,23 @@
+  var size = 250;
+  var box = document.getElementById("box");
 document.getElementById("button1").addEventListener("click", function(){
-  document.getElementById("box").style.height = "250px"; 
-  document.getElementById("box").style.width = "250px";
+  box.style.height = size+"px"; 
+  box.style.width = size+"px";
+  size = size * 1.25;
 });
 
 document.getElementById("button2").addEventListener("click", function(){
-  document.getElementById("box").style.backgroundColor = "blue"
+  box.style.backgroundColor = "blue";
 });
 
 document.getElementById("button3").addEventListener("click", function(){
-  document.getElementById("box").style.opacity = "0.5"
+  box.style.opacity = "0.5";
 });
 
 document.getElementById("button4").addEventListener("click", function(){
-  document.getElementById("box").style.height = "150px"
-  document.getElementById("box").style.width = "150px"
-  document.getElementById("box").style.backgroundColor = "orange"
-  document.getElementById("box").style.opacity = "1.0"
+  box.style.height = "150px"; 
+  box.style.width = "150px";
+  box.style.backgroundColor = "orange";
+  box.style.opacity = "1.0";
+  size = 250;
 });
