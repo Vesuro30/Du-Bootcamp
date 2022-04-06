79ef5a84a1e73fcea5427c3cc49c54e54053caae
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-04-06 13:28:08 -0600

    test

--- spot the differences/BrokenCode/logic.js
+++ spot the differences/BrokenCode/logic.js
@@ -3,10 +3,10 @@ $("#factButton").on("click", function() {
 	$("#factText").text(booFacts[number]);
 });
 
-var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass", "Boo has released two books" ]
+var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass", "Boo has released two books"];
 
 $("#textPink").on("click", function() {
-	$("#funText").css("color", pink);
+	$("#funText").css("color", "pink");
 });
 
 $("#textOrange").on("click", function() {
@@ -18,11 +18,22 @@ $("#textGreen").on("click", function() {
 });
 
 
-$("#boxGrow").on(click, function() {
-	$("#box").animate({height:"+=35px", 
-		width:"+=35px"}, "fast");
+$("#boxGrow").on("click", function() {
+	var box = 	$("#box");
+	var limit = 175;
+	box.animate({height:"+=35px", width:"+=35px"}, "1000", function(){
+		if(box.css("height").substr(0, 3) > limit) { 
+			box.css({"height": limit+"px", "width": limit+"px"});	
+		}
+	});
 });
 
-$("#boxShrink").on(click, function() {
-	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
+$("#boxShrink").on("click", function() {
+	var box = 	$("#box");
+	var limit = 100;
+	box.animate({height:"-=35px", width:"-=35px"}, "1000", function(){
+		if(box.css("height").substr(0, 3) < limit) { 
+			box.css({"height": limit+"px", "width": limit+"px"});	
+		}
+	});
 });
